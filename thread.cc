0389e1dfd5739ab8381fb166599f781433d09e13
Rafael Moresco Vieira <33913662+rafaelmoresco@users.noreply.github.com>
2022-05-17 08:59:14 -0300

    update feedback pt2

--- thread.cc
+++ thread.cc
@@ -6,19 +6,20 @@
 
 __BEGIN_API
     Thread * Thread::_running = nullptr;
+    static unsigned int Thread::_thread_counter = 0;
 
     /*
-     * Método para trocar o contexto entre duas thread, a anterior (prev)
-     * e a próxima (next).
+     * MÃ©todo para trocar o contexto entre duas thread, a anterior (prev)
+     * e a prÃ³xima (next).
      * Deve encapsular a chamada para a troca de contexto realizada pela class CPU.
-     * Valor de retorno é negativo se houve erro, ou zero.
+     * Valor de retorno Ã© negativo se houve erro, ou zero.
      */ 
     int Thread::switch_context(Thread * prev, Thread * next) {
         db<Thread>(TRC)<<"Thread::switch_context()\n";
         try {
-            CPU::switch_context(prev->_context, next->_context);
-            //Thread::setRunning(next);
             _running = next;
+			CPU::switch_context(prev->_context, next->_context);
+            //Thread::setRunning(next);
         } catch (...) {
             return -1;
         }
@@ -27,11 +28,12 @@ __BEGIN_API
 
     /*
      * Termina a thread.
-     * exit_code é o código de término devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
-     * Quando a thread encerra, o controle deve retornar à main. 
+     * exit_code Ã© o cÃ³digo de tÃ©rmino devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
+     * Quando a thread encerra, o controle deve retornar Ã  main. 
      */  
     void Thread::thread_exit (int exit_code) {
         db<Thread>(TRC)<<"Thread::exit()\n";
+        Thread::_thread_counter--;
         delete Thread::_context;
     }
 
@@ -44,7 +46,7 @@ __BEGIN_API
     }
 
     /*
-     * Qualquer outro método que você achar necessário para a solução.
+     * Qualquer outro mÃ©todo que vocÃª achar necessÃ¡rio para a soluÃ§Ã£o.
      */ 
     CPU::Context * volatile Thread::context() {
         db<Thread>(TRC)<<"Thread::context()\n";
