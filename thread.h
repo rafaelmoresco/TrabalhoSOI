7d3b2ddcbe07896b6e3a463b97d2106d51b34709
Rafael Moresco Vieira <33913662+rafaelmoresco@users.noreply.github.com>
2022-05-17 08:58:46 -0300

    update feedback pt1

--- thread.h
+++ thread.h
@@ -20,22 +20,23 @@ public:
     }
 
     /*
-     * Cria uma Thread passando um ponteiro para a função a ser executada
-     * e os parâmetros passados para a função, que podem variar.
+     * Cria uma Thread passando um ponteiro para a funÃ§Ã£o a ser executada
+     * e os parÃ¢metros passados para a funÃ§Ã£o, que podem variar.
      * Cria o contexto da Thread.
-     * PS: devido ao template, este método deve ser implementado neste mesmo arquivo .h
+     * PS: devido ao template, este mÃ©todo deve ser implementado neste mesmo arquivo .h
      */ 
     template<typename ... Tn>
-    Thread(void (* entry)(Tn ...), Tn ... an) {
+    inline Thread(void (* entry)(Tn ...), Tn ... an) {
         db<Thread>(TRC) << "Thread::Thread(void (* entry)(Tn ...), Tn ... an)\n";
-        _context = new Context(entry, an...);
-        if (!(_context)) {
+        _context = new Context(entry, (char *) an...);
+        _id = _thread_counter++;
+		if (!(_context)) {
             throw "falha em criar contexto";
         }
     }
 
     /*
-     * Retorna a Thread que está em execução.
+     * Retorna a Thread que estÃ¡ em execuÃ§Ã£o.
      */ 
     static Thread * running() { 
         db<Thread>(TRC)<<"Thread::running()\n";
@@ -43,17 +44,17 @@ public:
     }
 
     /*
-     * Método para trocar o contexto entre duas thread, a anterior (prev)
-     * e a próxima (next).
+     * MÃ©todo para trocar o contexto entre duas thread, a anterior (prev)
+     * e a prÃ³xima (next).
      * Deve encapsular a chamada para a troca de contexto realizada pela class CPU.
-     * Valor de retorno é negativo se houve erro, ou zero.
+     * Valor de retorno Ã© negativo se houve erro, ou zero.
      */ 
     static int switch_context(Thread * prev, Thread * next);
 
     /*
      * Termina a thread.
-     * exit_code é o código de término devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
-     * Quando a thread encerra, o controle deve retornar à main. 
+     * exit_code Ã© o cÃ³digo de tÃ©rmino devolvido pela tarefa (ignorar agora, vai ser usado mais tarde).
+     * Quando a thread encerra, o controle deve retornar Ã  main. 
      */  
     void thread_exit (int exit_code);
 
@@ -63,7 +64,7 @@ public:
     int id();
 
     /*
-     * Qualquer outro método que você achar necessário para a solução.
+     * Qualquer outro mÃ©todo que vocÃª achar necessÃ¡rio para a soluÃ§Ã£o.
      */
 
     Context * volatile context(); 
@@ -72,9 +73,10 @@ private:
     int _id;
     Context * volatile _context;
     static Thread * _running;
+    static unsigned int _thread_counter;
 
     /*
-     * Qualquer outro atributo que você achar necessário para a solução.
+     * Qualquer outro atributo que vocÃª achar necessÃ¡rio para a soluÃ§Ã£o.
      */ 
 };
 
